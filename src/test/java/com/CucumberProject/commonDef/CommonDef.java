fcb49e62d5af0fe57b464851219082fdd6670518
hrhrnHari <hariharana@maveric-systems.com>
2020-08-04 17:52:50 +0530

    sudden change

--- src/test/java/com/CucumberProject/commonDef/CommonDef.java
+++ src/test/java/com/CucumberProject/commonDef/CommonDef.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -36,6 +38,7 @@ import com.maveric.core.utils.reporter.Report;
 import com.maveric.core.utils.web.WebActions;
 
 public class CommonDef extends WebActions{
+	protected final static Logger logger = LogManager.getLogger();
 	
 	public  WebDriver getCurrentDriver()
 	{
